6b26de1b6f9c88ecc81f7e2a6f4f90736cd160db
zunbin <905192187@qq.com>
2019-03-24 13:03:52 +0800

    完成网关

--- product_service/src/main/java/com/wangzunbin/product_service/controller/ProductController.java
+++ product_service/src/main/java/com/wangzunbin/product_service/controller/ProductController.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * ClassName:ProductController  <br/>
  * Funtion: ${TODD} <br/>
@@ -35,12 +33,12 @@ public class ProductController {
 
     @RequestMapping("find")
     public Object findByid(Integer id){
-
+/*
         try {
             TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
+        }*/
         Product product = productService.findById(id);
         Product product1 = new Product();
         BeanUtils.copyProperties(product, product1);
